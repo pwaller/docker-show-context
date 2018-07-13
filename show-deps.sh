ae798e6f1c61d22146bb9d9f1ce3b319d6efbf78
Peter Waller <p@pwaller.net>
2018-07-13 13:23:13 +0100

    Remove show-deps.sh

--- show-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# A short utility to show packages used recursively.
-# Useful for spotting missed vendorings.
-
-go list -f '{{range .Deps}}{{.}}{{"\n"}}{{end}}' . |
-  xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' |
-  sort -u
